9040313b87ecff416389cd609152470c6c299266
Josh Rolstad <jorolsta@microsoft.com>
2020-06-15 07:01:16 -0700

    Adding script to publish

---
+++ src/publish.ps1
@@ -0,0 +1,18 @@
+﻿$isAdmin = [bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")
+
+if($isAdmin -eq $false){
+    throw "This script but be run with Administrator privileges"
+}
+
+$targetPath = "C:\Program Files (x86)\Steam\SteamApps\Common\Kerbal Space Program\GameData\MyKSPProjectPlugins"
+$sourcePath = join-path $PSScriptRoot "MyKSPProject\bin\Debug"
+
+$sourceLibrary = join-path $sourcePath "MyKSPProject.dll"
+
+if ((Test-Path $targetPath) -eq $false){
+    mkdir $targetPath
+} 
+
+Write-Host "Copying $sourceLibrary to $targetPath ..."
+Copy-Item -Path $sourceLibrary -Destination $targetPath -Force
+Write-Host "  Done"
